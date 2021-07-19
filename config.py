ee3d6bf7bb24ce735429a702df86d0a46c549697
James Clark <57755656+suusnale2019@users.noreply.github.com>
2021-07-19 21:33:18 +0530

    Update Config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ customuser = "empadmin"
 custompass = "Empadmin%2021"
 customdb = "employee"
 custombucket = "employyebuk"
-customregion = "us-west-2"
+customregion = "us-east-2"
 customtable = "employee"
 
