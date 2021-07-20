07b140a934a811a0a01661d13367a5838cdbecdf
James Clark <57755656+suusnale2019@users.noreply.github.com>
2021-07-20 16:11:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-customhost = "empdb.czyfpglofrby.us-east-2.rds.amazonaws.com"
+customhost = "empdb.czyfpglofrby.us-east-2.rds.amazonaws.com:3306"
 customuser = "empadmin"
 custompass = "Empadmin%2021"
 customdb = "employee"
 custombucket = "employyebuk"
-customregion = "us-east-2c"
+customregion = "us-east-2"
 customtable = "employee"
 
